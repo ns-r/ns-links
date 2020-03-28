c0b769b89f45ef17b324d4ae32fcdb4c22fa1034
themindstorm <parth.kabra@gmail.com>
2020-03-28 10:59:00 +0800

    added navbar.vue

---
+++ components/Navbar.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+  nav
+    .logo NSR
+    .right
+      nuxt-link(to="/links") Links
+      nuxt-link(to="/about") About
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
